7cf2b70e51d71d2182d36d3284aa5608e888388b
Kur01234 <matteo.ruf@icloud.com>
2024-08-07 17:14:59 +0200

    feat: fix updater initilaization

--- user-restriction.php
+++ user-restriction.php
@@ -22,23 +22,28 @@ $actions = array_merge( $mylinks, $actions );
 return $actions;
 }
 
-include_once('updater.php');
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-	$config = array(
-		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-		'proper_folder_name' => 'User-Restriction-WP', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/Kur01234/User-Restriction-WP', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/Kur01234/User-Restriction-WP/main', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/Kur01234/User-Restriction-WP', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/Kur01234/User-Restriction-WP/zipball/main', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-		'requires' => '3.0', // which version of WordPress does your plugin require?
-		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-		'readme' => 'README.md', // which file to use as the readme for the version number
-		'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-	);
-	new WP_GitHub_Updater($config);
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+			'proper_folder_name' => 'User-Restriction-WP', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/Kur01234/User-Restriction-WP', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/Kur01234/User-Restriction-WP/main', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/Kur01234/User-Restriction-WP', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/Kur01234/User-Restriction-WP/zipball/main', // the zip url of the GitHub repo
+			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.0', // which version of WordPress does your plugin require?
+			'tested' => '3.3', // which version of WordPress is your plugin tested up to?
+			'readme' => 'README.md', // which file to use as the readme for the version number
+			'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
+		);
+		new WP_GitHub_Updater($config);
+	}
 }
 
 function on_site_open() {
