908502d3044f7b27a9702ca26133a5b12ce004f5
Kur01234 <matteo.ruf@icloud.com>
2024-08-07 17:17:29 +0200

    feat: no real changes just testing the updater

--- user-restriction.php
+++ user-restriction.php
@@ -3,7 +3,7 @@
  * Plugin Name: Custom User Restriction
  * Author: Kuro
  * Author URI: https://github.com/Kur01234
- * Version: 1.1
+ * Version: 1.2
  * Description: Restrict acces to pages for All User Groups that are not selected.
  */
 
