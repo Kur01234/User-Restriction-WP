4f0e9346cda155eff9cbda5158d637c28f345059
Kur01234 <matteo.ruf@icloud.com>
2024-08-07 16:42:17 +0200

    feat: Url fix

--- user-restriction.php
+++ user-restriction.php
@@ -29,10 +29,10 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 	$config = array(
 		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 		'proper_folder_name' => 'User-Restriction-WP', // this is the name of the folder your plugin lives in
-		'api_url' => 'https://api.github.com/repos/username/repository-name', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/username/repository-name/master', // the GitHub raw url of your GitHub repo
-		'github_url' => 'https://github.com/username/repository-name', // the GitHub url of your GitHub repo
-		'zip_url' => 'https://github.com/username/repository-name/zipball/master', // the zip url of the GitHub repo
+		'api_url' => 'https://api.github.com/repos/Kur01234/User-Restriction-WP', // the GitHub API url of your GitHub repo
+		'raw_url' => 'https://raw.github.com/Kur01234/User-Restriction-WP/main', // the GitHub raw url of your GitHub repo
+		'github_url' => 'https://github.com/Kur01234/User-Restriction-WP', // the GitHub url of your GitHub repo
+		'zip_url' => 'https://github.com/Kur01234/User-Restriction-WP/zipball/main', // the zip url of the GitHub repo
 		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 		'requires' => '3.0', // which version of WordPress does your plugin require?
 		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
