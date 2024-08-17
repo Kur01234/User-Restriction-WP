f786f976e14f1ccf6ed15c512bca6c07455c5b77
Kur01234 <matteo.ruf@icloud.com>
2024-08-17 21:30:05 +0200

    feat: init assets

--- user-restriction.php
+++ user-restriction.php
@@ -3,7 +3,7 @@
  * Plugin Name: Custom User Restriction
  * Author: Kuro
  * Author URI: https://github.com/Kur01234
- * Version: 1.8
+ * Version: 1.9
  * Description: Restrict acces to pages for All User Groups that are not selected.
  */
 
